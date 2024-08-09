f9e598c8d2c41553d07c616468ba87853ff7b866
Anan Almasri <subscriptions@ananalmasri.com>
2024-08-08 21:17:49 -0400

    Production

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Anan's Calcualtor",
-  description: "The Best Islamic Inheretinace Calculator",
+  description: "Islamic Inheretinace Calculator",
 };
 
 export default function RootLayout({
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Calculator from '@/app/components/Calculator';
-import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
@@ -7,7 +6,6 @@ export default function Home() {
       <div className="flex flex-col justify-between h-full p-4 sm:p-6">
         <div className="flex-grow bg-white rounded shadow-md w-full max-w-sm mx-auto">
           <Calculator />
-          <Analytics />
         </div>
         <div className="h-24"></div>
       </div>
