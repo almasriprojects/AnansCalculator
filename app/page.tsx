a8197859a641006e93f4af0cf1df672d4eede962
Anan Almasri <subscriptions@ananalmasri.com>
2024-08-08 21:16:11 -0400

    Production

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Calculator from '@/app/components/Calculator';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <div className="flex flex-col justify-between h-full p-4 sm:p-6">
         <div className="flex-grow bg-white rounded shadow-md w-full max-w-sm mx-auto">
           <Calculator />
+          <Analytics />
         </div>
         <div className="h-24"></div>
       </div>
